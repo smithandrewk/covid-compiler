40df787c8e0e973f718e54ef32a0b66237b5f5cc
Andrew Smith <andrewsmith@sc.edu>
2025-01-15 09:47:01 -0500

    enhance readme, refactor main

--- README.md
+++ README.md
@@ -1,25 +1,76 @@
-1. Don't think the pangolin install works on mac
-2. On Linux
+1. On a Linux machine, open a new terminal
+2. Clone repository
+```
+git clone https://github.com/smithandrewk/covid-compiler.git
+```
+3. Enter directory
+```
+cd covid-compiler
+```
 3. Download and install miniconda
 ```
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda*
 ```
-4. Create old conda env
-   ```
-   conda create --name pangolin_env python=3.8
-	```
+4. Create old conda env (Panoglin maintainters haven't updated)
+```
+conda create --name pangolin_env python=3.8
+```
 5. Activate
-   ```
-   conda activate pangolin_env
-	```
+```
+conda activate pangolin_env
+```
 6. Install pangolin
-   ```
-   conda install -c bioconda -c conda-forge -c defaults pangolin
-	```
+```
+conda install -c bioconda -c conda-forge -c defaults pangolin
+```
+6. Download sequencing log and place in covid-compiler directoy
+7. Download Fasta Files Folders and place in covid compiler directory
+Example Directory Structure:
+```
+(pangolin_env) andrew@tau:~/covid-compiler$ tree
+.
+├── 2024_COVID-19_sequencing_log.xlsx
+├── FastaFiles_01.06.2025
+│   └── 01.06.2025.fasta
+├── FastaFiles_09.16.2024
+│   └── 09.16.2024.fasta
+├── main.py
+└── README.md
 
-6. Download sequencing log
-7. Download Fasta Files Folders
+2 directories, 5 files
+```
 8. Pangolin each folder separately
-9. Run main.ipynb
+```
+cd FastaFiles_01.06.2025
+pangolin 01.06.2025.fasta
+cd ../
+cd FastaFiles_09.16.2024
+pangolin 09.16.2024.fasta
+cd ../
+```
+Directory Structure:
+```
+(pangolin_env) andrew@tau:~/covid-compiler$ tree
+.
+├── 2024_COVID-19_sequencing_log.xlsx
+├── FastaFiles_01.06.2025
+│   ├── 01.06.2025.fasta
+│   └── lineage_report.csv
+├── FastaFiles_09.16.2024
+│   ├── 09.16.2024.fasta
+│   └── lineage_report.csv
+├── main.py
+└── README.md
+
+2 directories, 7 files
+```
+9. Install dependency
+```
+pip3 install openpyxl
+```
+10. Run main.py
+```
+python3 main.py
+```
 10. Pray
--- main.py
+++ main.py
@@ -4,32 +4,15 @@ import pandas as pd
 # Find all directories that contain 'FastaFile' in their name
 dirs = [file for file in os.listdir('.') if 'FastaFile' in file]
 
-# Initialize an empty list to store DataFrames
-df_list = []
-
-# Loop through each directory, read the CSV, and append it to the list
-for dir in dirs:
-    csv_path = os.path.join(dir, 'lineage_report.csv')
-    df = pd.read_csv(csv_path)
-    df = df[['taxon', 'lineage']]
-    df_list.append(df)
-    print(f'{len(df)} new sequences from {dir}')
-
-# Concatenate all DataFrames into one
-final_df = pd.concat(df_list, ignore_index=True)
+# Loop through each directory, read the lineage_report CSV, and append it to the list
+lab_id_to_covid_lineage_df = pd.concat([pd.read_csv(os.path.join(dir, 'lineage_report.csv'))[['taxon', 'lineage']] for dir in dirs],ignore_index=True)
 
 log = pd.read_excel(f'2024_COVID-19_sequencing_log.xlsx')
-log = log.set_index('Key').loc[final_df['taxon'].to_list()]
+log = log.set_index('Key').loc[lab_id_to_covid_lineage_df['taxon'].to_list()]
+lab_id_to_covid_lineage_df = lab_id_to_covid_lineage_df.rename({'taxon':'Key'},axis=1).set_index('Key')
 print(f'{len(log)} found in log')
-log
-# if not equal big error
 
-final_df = final_df.rename({'taxon':'Key'},axis=1).set_index('Key')
-
-# Display the concatenated DataFrame
-print(f'Total sequences: {len(final_df)}')
-
-log['Variant'] = final_df['lineage']
+log['Variant'] = lab_id_to_covid_lineage_df['lineage']
 log['Category (VOI/VOC/other)'] = 'VOC'
 log['WHO Nomenclature '] = ''
 log['Variant Description'] = ''
@@ -37,6 +20,8 @@ log['Disease Onset Date'] = ''
 log['Notes'] = ''
 log['Lab'] = 'PHL'
 log = log.rename({'PATIENT NAME':'NAME','OPENELIS ACCESSION #':'OE #',"Date Rcd\'v in Molecular":'Date Discovered/Recieved'},axis=1)
-tmp = log[['Variant', 'WHO Nomenclature ', 'Variant Description','Category (VOI/VOC/other)', 'NAME', 'DOB', 'SEX', 'RACE', 'COUNTY','STATE', 'DOC', 'AGE', 'Disease Onset Date', 'OE #', 'Notes', 'Lab','Date Discovered/Recieved']]
-tmp['DOB'] = tmp['DOB'].astype('datetime64[ns]')
-tmp.to_csv(f'01_14_2025_Variants.csv',index=False)
+
+final_csv_to_send = log[['Variant', 'WHO Nomenclature ', 'Variant Description','Category (VOI/VOC/other)', 'NAME', 'DOB', 'SEX', 'RACE', 'COUNTY','STATE', 'DOC', 'AGE', 'Disease Onset Date', 'OE #', 'Notes', 'Lab','Date Discovered/Recieved']]
+final_csv_to_send['DOB'] = final_csv_to_send['DOB'].astype('datetime64[ns]')
+
+final_csv_to_send.to_csv(f'01_14_2025_Variants.csv',index=False)
